2f862d20b41d7f1b359d6bb8478776b83a5c93b3
Gabriel Vieira <gabrielvieira600@gmail.com>
2026-04-17 14:22:05 -0300

    brasileirão fix

--- routes/classificacao.js
+++ routes/classificacao.js
@@ -343,7 +343,7 @@ router.get('/tabela-brasileirao', async (req, res) => {
        Accept: 'application/json',
        },
       params: {
-        league: 140,
+        league: 71,
         season: 2025
       }
     });
