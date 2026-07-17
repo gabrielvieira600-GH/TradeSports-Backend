7319c3edc4e679288e2e42d2320b8474aa3c56e6
Gabriel Vieira <gabrielvieira600@gmail.com>
2026-07-17 16:39:30 -0300

    crase adicionado

--- utils/tradingQuota.js
+++ utils/tradingQuota.js
@@ -54,7 +54,7 @@ function criarDataBrasiliaInicioDoDia({ ano, mes, dia }) {
 
   return new Date(
 
-    '${ano}-${pad2(mes)}-${pad2(dia)}T00:00:00.000-03:00'
+    `${ano}-${pad2(mes)}-${pad2(dia)}T00:00:00.000-03:00`
 
   );
 
