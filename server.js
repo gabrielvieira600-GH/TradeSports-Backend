59374ff29bb85af1699bbbb3377672be42a70956
Gabriel Vieira <gabrielvieira600@gmail.com>
2026-09-16 12:10:09 -0300

    ...

--- server.js
+++ server.js
@@ -67,10 +67,10 @@ const socialRoutes = require("./routes/social");
 const rankingConvitesRoutes = require("./routes/rankingConvites");
 const socialCommunityRoutes = require('./routes/socialCommunity');
 const socialModerationRoutes = require("./routes/api/socialModeration");
-const socialCommentsV2Routes = require("./routes/socialCommentsV2");
 const trophiesRoutes = require("./routes/trophies");
 const socialOwnershipRoutes = require("./routes/socialOwnership");
 const socialCommentsV2Routes = require("./routes/socialCommentsV2");
+
 const {
   router: recoveryRechargeRoutes,
   stripeWebhook: recoveryRechargeStripeWebhook,
@@ -282,9 +282,11 @@ app.use("/social", socialRoutes);
 app.use("/ranking-convites", rankingConvitesRoutes);
 app.use("/trofeus", trophiesRoutes);
 app.use('/recarga-recuperacao', recoveryRechargeRoutes);
+app.use('/social-community', socialOwnershipRoutes);
 app.use('/social-community', socialCommentsV2Routes);
 app.use('/social-community', socialCommunityRoutes);
 app.use("/api/admin/social-moderation", socialModerationRoutes);
+
 
 if (watchlistRoutes) {
   app.use("/watchlist", watchlistRoutes);
